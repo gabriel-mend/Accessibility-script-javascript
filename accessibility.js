121b100daa554893c00fca17a9320c1c516420e6
Gabriel <gabriel@Gabriel-Mac-mini.local>
2020-02-04 10:27:34 -0300

    Js

--- accessibility.js
+++ accessibility.js
@@ -11,14 +11,14 @@ let h6 = document.querySelectorAll('h6')
 let span = document.querySelectorAll('span')
 let strong = document.querySelectorAll('strong')
 
-
-function pickFontSize(item) {
-    let array = []
+const pickFontSize = (item) => {
+    let arrayItems = []
     item.forEach((value, index) => {
-        array.push(getComputedStyle(item[index]).getPropertyValue('font-size'))
+        arrayItems.push(getComputedStyle(item[index]).getPropertyValue('font-size'))
     })
-    return array
+    return arrayItems
 }
+
 const pFontInicial = pickFontSize(p)
 const h1FontInicial = pickFontSize(h1)
 const h2FontInicial = pickFontSize(h2)
@@ -28,53 +28,26 @@ const h5FontInicial = pickFontSize(h5)
 const h6FontInicial = pickFontSize(h6)
 const spanFontInicial = pickFontSize(span)
 const strongFontInicial = pickFontSize(strong)
-function fontSizeCalculate(item) {
-    let array = []
-    item.forEach((value, index) => {
-        array.push(getComputedStyle(item[index]).getPropertyValue('font-size'))
-    })
 
-    let fontSizesArray = parseFontToInt(array).map(value => value * 0.2)
+const fontSizeCalculate = (item) => {
+    let arrayItems = pickFontSize(item)
+    let fontSizesArray = parseFontToInt(arrayItems).map(value => value * 0.2)
 
     return fontSizesArray
 }
 
-function parseFontToInt(item) {
+const parseFontToInt = (item) => {
     return item.map((value) => parseInt(value.replace('px', '')))
 }
 
-function returnFontSizeIntArray(item) {
-    let array = []
-    item.forEach((value, index) => {
-        array.push(getComputedStyle(item[index]).getPropertyValue('font-size'))
-    })
-
-    let fontSizesArray = parseFontToInt(array)
-    return fontSizesArray
-
-}
-
-
-function toArray(item) {
+const toArray = (item) => {
     return Array.from(item)
 }
 
-let pArray = toArray(p)
-let h1Array = toArray(h1)
-let h2Array = toArray(h2)
-let h3Array = toArray(h3)
-let h4Array = toArray(h4)
-let h5Array = toArray(h5)
-let h6Array = toArray(h6)
-let spanArray = toArray(span)
-let strongArray = toArray(strong)
+const addFontSize = (itemHtml) =>{
+        let itemArray = toArray(itemHtml)
 
-function addFontSize(itemArray, itemHtml) {
-        let array = []
-
-        itemArray.forEach((value, index) => {
-            array.push(getComputedStyle(itemArray[index]).getPropertyValue('font-size'))
-        })
+        let array = pickFontSize(itemArray)
 
         let fontInicial = fontSizeCalculate(itemHtml)
 
@@ -83,13 +56,12 @@ function addFontSize(itemArray, itemHtml) {
         itemHtml.forEach((item, index) => item.style.fontSize =`${ fontSizesArray[index] + fontInicial[index] }px`)
 
         return itemHtml
-}
+} 
 
-function removeFontSize(itemArray, itemHtml) {
-    let array = []
-    itemArray.forEach((value, index) => {
-        array.push(getComputedStyle(itemArray[index]).getPropertyValue('font-size'))
-    })
+const removeFontSize = (itemHtml) => {
+    let itemArray = toArray(itemHtml)
+
+    let array = pickFontSize(itemArray)
 
     let fontInicial = fontSizeCalculate(itemHtml)
 
@@ -99,49 +71,41 @@ function removeFontSize(itemArray, itemHtml) {
 
     return itemHtml
 }
-function resetFontSize(itemArray, itemHtml, itemInicial) {
-    let array = []
-
-    itemArray.forEach((value, index) => {
-        array.push(getComputedStyle(itemArray[index]).getPropertyValue('font-size'))
-    })
-    
+const resetFontSize = (itemHtml, itemInicial) => {
     itemHtml.forEach((item, index) => item.style.fontSize = itemInicial[index])
     return itemHtml
 }
-
+let count = 0
 increaseFontSize.onclick = () => {
-        addFontSize(pArray, p)
-        addFontSize(h1Array, h1)
-        addFontSize(h2Array, h2)
-        addFontSize(h3Array, h3)
-        addFontSize(h4Array, h4)
-        addFontSize(h5Array, h5)
-        addFontSize(h6Array, h6)
-        addFontSize(spanArray, span)
-        addFontSize(strongArray, strong)
+        addFontSize(p)
+        addFontSize(h1)
+        addFontSize(h2)
+        addFontSize(h3)
+        addFontSize(h4)
+        addFontSize(h5)
+        addFontSize(h6)
+        addFontSize(span)
+        addFontSize(strong)
 }
-
-
 decreaseFontSize.onclick = () => {
-    removeFontSize(pArray, p)
-    removeFontSize(h1Array, h1)
-    removeFontSize(h2Array, h2)
-    removeFontSize(h3Array, h3)
-    removeFontSize(h4Array, h4)
-    removeFontSize(h5Array, h5)
-    removeFontSize(h6Array, h6)
-    removeFontSize(spanArray, span)
-    removeFontSize(strongArray, strong)
+    removeFontSize(p)
+    removeFontSize(h1)
+    removeFontSize(h2)
+    removeFontSize(h3)
+    removeFontSize(h4)
+    removeFontSize(h5)
+    removeFontSize(h6)
+    removeFontSize(span)
+    removeFontSize(strong)
 }
 resetButton.onclick = () => {
-    resetFontSize(pArray, p, pFontInicial)
-    resetFontSize(h1Array, h1, h1FontInicial)
-    resetFontSize(h2Array, h2, h2FontInicial)
-    resetFontSize(h3Array, h3, h3FontInicial)
-    resetFontSize(h4Array, h4, h4FontInicial)
-    resetFontSize(h5Array, h5, h5FontInicial)
-    resetFontSize(h6Array, h6, h6FontInicial)
-    resetFontSize(spanArray, span, spanFontInicial)
-    resetFontSize(strongArray, strong, strongFontInicial)
+    resetFontSize(p, pFontInicial)
+    resetFontSize(h1, h1FontInicial)
+    resetFontSize(h2, h2FontInicial)
+    resetFontSize(h3, h3FontInicial)
+    resetFontSize(h4, h4FontInicial)
+    resetFontSize(h5, h5FontInicial)
+    resetFontSize(h6, h6FontInicial)
+    resetFontSize(span, spanFontInicial)
+    resetFontSize(strong, strongFontInicial)
 }
